96b52d37e229abed3121c4eda9cf281acec0f5e5
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-07-08 16:08:24 -0300

    render tags/styled components

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,19 +1,35 @@
 import Tag from '../Tag'
 
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Titulo, Infos } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => (
   <Card>
-    <img src="https://via.placeholder.com/222x250" alt="Imagem do produto" />
-    <Titulo>Nome do Jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, veniam? Modi
-      officia nesciunt explicabo esse blanditiis iste, cupiditate repellat illo
-      fugit distinctio perspiciatis voluptates iure nostrum quas optio quia
-      deserunt!
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((info) => (
+        <Tag key={info}>{info}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -2,20 +2,48 @@ import Product from '../Product'
 
 import { Container, List } from './styles'
 
-type Props = {
+export type Props = {
   title: string
   background: 'gray' | 'black'
 }
 
 const ProductsList = ({ background, title }: Props) => (
-  <Container>
+  <Container background={background}>
     <div className="container">
       <h2>{title}</h2>
       <List>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//via.placeholder.com/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do Jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//via.placeholder.com/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do Jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//via.placeholder.com/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do Jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//via.placeholder.com/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do Jogo"
+        />
       </List>
     </div>
   </Container>
