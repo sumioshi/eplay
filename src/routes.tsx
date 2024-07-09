535abd120ae45e0ac55497146bd287a7be168c2d
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-07-09 16:33:57 -0300

    product id

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Categories from './pages/Categories'
+import Product from './pages/Product'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categorias" element={<Categories />} />
+    <Route path="/product/:id" element={<Categories />} />
   </Routes>
 )
 
