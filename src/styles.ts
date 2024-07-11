79c9f1025c3be23a604dbb0529898e42f580f26a
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-07-11 17:30:35 -0300

    responsividade

--- src/styles.ts
+++ src/styles.ts
@@ -8,6 +8,11 @@ export const cores = {
   cinzaClaro: '#A3A3A3'
 }
 
+export const breakpoints = {
+  desktop: '1024px',
+  tablet: '768px'
+}
+
 export const GlobalCss = createGlobalStyle`
   * {
     margin: 0;
@@ -21,11 +26,15 @@ export const GlobalCss = createGlobalStyle`
     background-color: ${cores.preta};
     color: ${cores.branca};
     padding-top: 40px;
+  }
 
     .container {
     max-width: 1024px;
     width: 100%;
     margin: 0 auto;
+
+    @media (max-width: ${breakpoints.desktop}) {
+    max-width: 80%;
     }
   }
 `
